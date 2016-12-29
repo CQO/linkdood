bcaa9d90fb3b04a9df8e27e2e6434d5cf216be06
PUGE <my@owo.help>
2016-12-29 14:41:10 +0800

    配置jslint

--- config.js
+++ config.js
@@ -9,19 +9,19 @@ let config = {
 
   // webpack-dev-server port
   port: 9080,
-  icon16: path.join(__dirname, 'app/icons', 'icon16.png'),
+  icon16: path.join(__dirname, 'app/icons', 'icon16.png'),// jshint ignore:line
 
   // electron-packager options
   // Docs: https://simulatedgreg.gitbooks.io/electron-vue/content/docs/building_your_app.html
   building: {
     arch: 'x64',
     asar: true,
-    dir: path.join(__dirname, 'app'),
-    icon: path.join(__dirname, 'app/icons/icon'),
+    dir: path.join(__dirname, 'app'),// jshint ignore:line
+    icon: path.join(__dirname, 'app/icons/icon'),// jshint ignore:line
     ignore: /\b(node_modules|src|index\.ejs|icons)\b/,
-    out: path.join(__dirname, 'builds'),
+    out: path.join(__dirname, 'builds'),// jshint ignore:line
     overwrite: true,
-    platform: process.env.PLATFORM_TARGET || 'all'
+    platform: process.env.PLATFORM_TARGET || 'all'// jshint ignore:line
   }
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,12 @@ let config = {
   devtool: '#eval-source-map',
   //指入口文件的配置项，它是一个数组的原因是webpack允许多个入口点。 当然如果你只有一个入口的话，也可以直接使用双引号"./entry.js"
   entry: {
-    build: path.join(__dirname, 'app/src/main.js')
+    build: path.join(__dirname, 'app/src/main.js')// jshint ignore:line
   },
   //配置打包结果，path定义了输出的文件夹，filename则定义了打包结果文件的名称
   output: {
     filename: '[name].js',
-    path: path.join(__dirname, 'app/dist')
+    path: path.join(__dirname, 'app/dist')// jshint ignore:line
   },
   //定义了对模块的处理逻辑，这里可以用loaders定义了一系列的加载器，以及一些正则。
   //当需要加载的文件匹配test的正则时，就会调用后面的loader对文件进行处理，这正是webpack强大的原因。
@@ -38,12 +38,12 @@ let config = {
   ],
   //其它解决方案配置
   resolve: {
-    alias: {'components': path.join(__dirname, 'app/src/components') , 'src': path.join(__dirname, 'app/src')},
+    alias: {'components': path.join(__dirname, 'app/src/components') , 'src': path.join(__dirname, 'app/src')},// jshint ignore:line
     extensions: ['', '.js', '.vue', '.json', '.css'],
-    fallback: [path.join(__dirname, 'app/node_modules')]
+    fallback: [path.join(__dirname, 'app/node_modules')]// jshint ignore:line
   },
   resolveLoader: {
-    root: path.join(__dirname, 'node_modules')
+    root: path.join(__dirname, 'node_modules')// jshint ignore:line
   },
   target: 'electron-renderer',
   vue: {
@@ -57,7 +57,7 @@ let config = {
 /**
  * Adjust config for production settings
  */
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {// jshint ignore:line
   config.devtool = '';
 
   config.plugins.push(
