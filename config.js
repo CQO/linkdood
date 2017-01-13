636ff157c00ad2e718a6b634a3ec105263d39f79
PUGE <my@owo.help>
2017-01-13 11:28:07 +0800

    修复正常下载包

--- config.js
+++ config.js
@@ -9,19 +9,19 @@ let config = {
 
   // webpack-dev-server port
   port: 9080,
-  icon16: path.join(__dirname, 'app/icons', 'icon16.png'),// jshint ignore:line
+  icon16: path.join(__dirname, 'app/icons', 'icon16.png'),
 
   // electron-packager options
   // Docs: https://simulatedgreg.gitbooks.io/electron-vue/content/docs/building_your_app.html
   building: {
     arch: 'x64',
     asar: true,
-    dir: path.join(__dirname, 'app'),// jshint ignore:line
-    icon: path.join(__dirname, 'app/icons/icon'),// jshint ignore:line
+    dir: path.join(__dirname, 'app'),
+    icon: path.join(__dirname, 'app/icons/icon'),
     ignore: /\b(node_modules|src|index\.ejs|icons)\b/,
-    out: path.join(__dirname, 'builds'),// jshint ignore:line
+    out: path.join(__dirname, 'builds'),
     overwrite: true,
-    platform: process.env.PLATFORM_TARGET || 'all'// jshint ignore:line
+    platform: process.env.PLATFORM_TARGET || 'all'
   }
 };
 
