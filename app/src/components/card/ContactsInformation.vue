26e609c0ebeeb95bbcd6c27a264980feb5e346cc
PUGE <my@owo.help>
2017-01-25 13:33:01 +0800

    更新详情页

--- app/src/components/card/ContactsInformation.vue
+++ app/src/components/card/ContactsInformation.vue
@@ -8,15 +8,20 @@
         position: absolute;
         width: 250px;
         top: 25%;
-    left: 51%;
+        left: 51%;
         .user-img{
             height: 100px;
-            width: 116px;
-            margin: 0 auto;
+            width: 100px;
+            margin: 10px auto;
         }
         .user-name{
             height: 30px;
             line-height: 30px;
+        }
+        .man:after{
+            content:"♂";
+            color: #c39145;
+                padding-left: 10px;
         }
         .buttom{
             background-color: #2c89f2;
@@ -34,7 +39,7 @@
     .contacts-information-box
         .box
             Avatar.user-img(v-bind:size='100',v-bind:username="$route.params.item")
-            .user-name {{ $route.params.item }}
+            .user-name.man {{ $route.params.item }}
             router-link.buttom(v-bind:to="'/chat/chatToPeople/'+$route.params.item") 发起聊天 
 </template>
 <script>
