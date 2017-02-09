ce78f75b78d31ac7c32bafaf795a14370315a40f
PUGE <my@owo.help>
2017-02-09 18:03:45 +0800

    插件

--- app/src/renderer/components/More.vue
+++ app/src/renderer/components/More.vue
@@ -1,51 +1,63 @@
 <style lang="postcss">
 .application{
     .application-box{
-        background-color: #d7ebff;
         height: 80%;
-        width: 666px;
         margin: 7% auto;
+        display: flex;
+        flex-wrap: wrap;
+        width: 42rem;
         .box{
             width: 220px;
             height: 50%;
             float: left;
             border: 1px solid beige;
         }
+        .Aquamarine{
+            background-color: Aquamarine
+        }
+        .Hotpink{
+            background-color: Hotpink
+        }
+        .Gold{
+            background-color: Gold
+        }
+        .Crimson{
+            background-color: Crimson
+        }
+        .Blueviolet{
+            background-color: Blueviolet
+        }
+        .Lightblue{
+            background-color: Lightblue
+        }
     }
 }
 </style>
 <template lang="pug">
     .application
-        .application-box
-            .box(@click.prevent.stop="denglu") 登陆
-            .box(@click.prevent.stop="haoyou") 获取好友列表
-            .box(@click.prevent.stop="baocun") 保存聊天记录
-            .box
-            .box
-            .box
+        kkk.application-box
+            template(v-for="color in colors")
+                box.box(v-dragging="{ item: color, list: colors, group: 'color' }",v-bind:class="color.text") {{color.text}}
 </template>
 <script>
     import fun from './module/fun'
     export default {
-        methods:{
-            denglu(){
-                alert("开始登陆")
-                fun.Ajax.post("http://192.168.132.217:3000/","Landing",function(e){
-                    console.log(e);
-                    alert(e);
-                })
-            },
-            haoyou(){
-                alert("开始获取好友")
-                fun.Ajax.post("http://192.168.132.217:3000/","friendsList",function(e){
-                    console.log(e);
-                    alert(e);
-                })
-            },
-            baocun(){
-                alert("开始保存聊天记录")
-                this.$store.commit("SAVE_YOUR_CHATS")
+        data () {
+            return {
+                colors: [{
+                    text: "Aquamarine"
+                }, {
+                    text: "Hotpink"
+                }, {
+                    text: "Gold"
+                }, {
+                    text: "Crimson"
+                }, {
+                    text: "Blueviolet"
+                }, {
+                    text: "Lightblue"
+                }]
             }
-        },
+        }
     }
 </script>
