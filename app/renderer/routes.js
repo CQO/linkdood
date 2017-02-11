9f5befc1b498c37a3cfc7e2fd19e9e90db9e784a
PUGE <mmit7750>
2017-02-11 13:04:46 +0800

    save

--- app/renderer/routes.js
+++ app/renderer/routes.js
@@ -4,23 +4,17 @@ export default [
     path: '/',
     component: require('components/list/ChatList'),
     children:[
-      {
-        path: 'chat',
-        component: require('components/list/ChatList'),
-        children:[
-          {path: 'chatToPeople/:item/:id',component: require('components/MessageBox')}
-        ]
-      },
-      {
-        path: 'contacts',
-        component: require('components/list/ContactsList'),
-        children:[
-          {path: 'userIntroduction/:item/:id',component: require('components/card/ContactsInformation')}
-        ]
-      },
-      {path: 'more',component: require('components/More')},
+      {path: 'chatToPeople/:item/:id',component: require('components/MessageBox')}
     ]
   },
+  {
+    path: '/contacts',
+    component: require('components/list/ContactsList'),
+    children:[
+      {path: 'userIntroduction/:item/:id',component: require('components/card/ContactsInformation')}
+    ]
+  },
+  {path: '/more',component: require('components/More')},
   {
     path: '/Screenshot',
     component: require('components/tools/Screenshot')
