d7fc58b18f7ffdbc49b22764a1399291f989b8b3
PUGE <mmit7750>
2017-02-11 15:44:22 +0800

    气泡样式

--- app/renderer/components/bubble/default.vue
+++ app/renderer/components/bubble/default.vue
@@ -1,3 +1,68 @@
+<style lang="postcss">
+.message-left,.message-right{
+    margin: 10px;
+    height: initial;
+    .message{
+        width: 120px; 
+        height: 45px; 
+        position: relative; 
+        border-radius: 10px; 
+        width: initial;
+        padding: 10px;
+        height: initial;
+        min-height: 25px;
+        line-height: 25px;
+        max-width: 300px;
+        -webkit-user-select: text;
+      }
+    .message:before{
+        content: "";
+        position: absolute;
+        top: 16px;
+        width: 0;
+        height: 0;
+        border-top: 7px solid transparent;
+        border-bottom: 7px solid transparent;     
+    }
+    .user-img{
+        height: 45px;
+        width: 45px;
+        cursor: pointer;
+    }
+}
+.message-left{
+    float: left;
+    .message{
+        background: #eeeeee; 
+        margin-left: 20px;
+        float: left;
+    }
+    .message:before{
+        right: 100%;
+        border-right: 14px solid #eeeeee;
+    }
+    .user-img{
+        height: 45px;
+        width: 45px;
+        float: left;
+    }
+}
+.message-right{
+    float: right;
+    .message{
+        background-color: #d0edfe;
+        margin-right: 20px;
+        float: right;
+    }
+    .message:before{
+        left: 100%;
+        border-left: 14px solid #d0edfe;
+    }
+    img{
+        float: right;
+    }
+}
+</style>
 <template lang="pug">
     .bubble(v-bind:class="[left ? 'message-right' : 'message-left']")
         img.user-img(v-bind:src="userImg")
